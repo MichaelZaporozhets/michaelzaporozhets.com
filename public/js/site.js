ae6add4d41e2f05e55c6f82997744e44cd3d0eea
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2016-06-12 18:14:26 +1000

    fixed fallback support

--- public/js/site.js
+++ public/js/site.js
@@ -2,20 +2,7 @@ $(document).ready(function() {
 	$('.nav ul li a').click(function(e) {
 		e.preventDefault();
 
-		var currentPage = $('.page.cur').attr('data-name');
-		var nextPage = $(this).attr('data-name');
-
-		$('.page.cur, .nav ul li').removeClass('cur');
-		$(this).parent().addClass('cur');
-
-		$('.page[data-name="'+nextPage+'"]').addClass('cur');
-		$('.page[data-name="'+currentPage+'"] .animBlock').removeClass('run');
-		$('.page[data-name="'+nextPage+'"] .animBlock').each(function(i) {
-			var $this = $(this);
-			setTimeout(function() {
-				$this.addClass('run');
-			}, 200*(i+1));
-		});
+		SH.open($(this).attr('data-name'))
 
 
 		// $('.page[data-name="'+currentPage+'"]').addClass('transitionout');
@@ -41,30 +28,47 @@ $(document).ready(function() {
 
 	});
 
-	$('.nav ul li.cur a').click();
+
+
+	SH.pages = {
+		home: [],
+		cv: [],
+		portfolio: [],
+		contact: [],
+		pagenotfound: []
+	}
+
+	SH.firstPage = true;
+	SH.loadedFirst = false;
+
+	var openPage = function(page, args) {
+		var currentPage = $('.page.cur').attr('data-name');
+
+		var nextPage = page;
+
+		if(currentPage == nextPage) return false;
+
+		if(nextPage == '') nextPage = 'home'
+
+		$('.page.cur, .nav ul li').removeClass('cur');
+		$('.nav ul li a[data-name="'+nextPage+'"]').parent().addClass('cur');
+
+		$('.page[data-name="'+nextPage+'"]').addClass('cur');
+		$('.page[data-name="'+currentPage+'"] .animBlock').removeClass('run');
+		$('.page[data-name="'+nextPage+'"] .animBlock').each(function(i) {
+			var $this = $(this);
+			setTimeout(function() {
+				$this.addClass('run');
+			}, 200*(i+1));
+		});
+
+
+		SH.firstPage = false;
+		return false;
+	};
+
+	SH.info.root = '';
+	SH.init(openPage, {});
 
 	$('#body').css('min-height', window.innerHeight);
 });
-
-
-
-SH.pages = {
-	home: [],
-	cv: [],
-	portfolio: [],
-	contact: [],
-	pagenotfound: []
-}
-
-SH.firstPage = true;
-SH.loadedFirst = false;
-
-var openPage = function(page, args) {
-	$('.nav ul li a[data-name="'+page+'"]').click();
-
-	SH.firstPage = false;
-	return false;
-};
-
-SH.info.root = '';
-SH.init(openPage, {});
