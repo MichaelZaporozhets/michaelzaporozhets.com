64a34a179fab9eb0ac1fc50295651664939e7789
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2017-07-18 09:56:50 +1000

    remove email

--- keystone.js
+++ keystone.js
@@ -16,8 +16,6 @@ keystone.init({
 	'views': 'templates/views',
 	'view engine': 'jade',
 
-	'emails': 'templates/emails',
-
 	'auto update': true,
 	'session': true,
 	'auth': true,
@@ -35,32 +33,6 @@ keystone.set('locals', {
 });
 
 keystone.set('routes', require('./routes'));
-
-
-keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7'
-		}
-	}
-});
-
-keystone.set('email rules', [{
-	find: '/images/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
-}, {
-	find: '/keystone/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
-}]);
-
-keystone.set('email tests', require('./routes/emails'));
 
 keystone.set('nav', {
 	'posts': ['posts', 'post-categories'],
